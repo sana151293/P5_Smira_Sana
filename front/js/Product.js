7bcd6251e8a949482bb71bf8c8dee436033c2b9e
sana <smira.sana@hotmail.fr>
2022-11-13 16:06:10 +0100

    add files front

--- front/js/Product.js
+++ front/js/Product.js
@@ -1,19 +1,24 @@
-// récupère l'id
-const str = window.location.href;
-const url = new URL(str);
-const id = url.searchParams.get("id");
+const productId = getProductIdFromUrl();
 
-let productData = [];
-fetch(`http://localhost:3000/api/products/${id}`) // 5s
+const item = JSON.parse(localStorage.getItem("item")) || [];
+fetch(`http://localhost:3000/api/products/${productId}`)
   .then((response) => response.json())
   .then((product) => {
-    productData = product;
-    console.log(productData)
-    displayProductInformation();
-  })
+    displayProductInformationInHtml(product);
+    addCart(product);
+   
+  }).catch((err) => {
+    alert(err)
+  });
 
 
-const displayProductInformation = () => {
+function getProductIdFromUrl() {
+  const str = window.location.href;
+  const url = new URL(str);
+  return url.searchParams.get("id");
+}
+
+const displayProductInformationInHtml = (product) => {
 
   const pageTitleDiv = document.querySelector("head > title");
   const imgDiv = document.querySelector(".item__img");
@@ -23,42 +28,41 @@ const displayProductInformation = () => {
   const colorsDiv = document.getElementById('colors');
 
   // Intégration dans la page HTML
-  pageTitleDiv.innerHTML = productData.name;
-  imgDiv.innerHTML += `<img src="${productData.imageUrl}" alt="${productData.altTxt}">`;
-  titleDiv.innerHTML += productData.name;
-  priceDiv.innerHTML += productData.price;
-  descriptionDiv.innerHTML += productData.description;
+  pageTitleDiv.innerHTML = product.name;
+  imgDiv.innerHTML += `<img src="${product.imageUrl}" alt="${product.altTxt}">`;
+  titleDiv.innerHTML += product.name;
+  priceDiv.innerHTML += product.price;
+  descriptionDiv.innerHTML += product.description;
 
-  for (let i = 0; i < productData.colors.length; i++) {
+  for (let i = 0; i < product.colors.length; i++) {
     colorsOption = document.createElement('option');
-    colorsOption.text = productData.colors[i];
+    colorsOption.text = product.colors[i];
     colorsDiv.add(colorsOption);
-
   }
-
-  addBasket()
+  
 };
-function saveInLocalStorage(product) {
-  let productInLocalStorage = JSON.parse(localStorage.getItem("item"));
-  if (productInLocalStorage === null) {
-      productInLocalStorage= [];
-      productInLocalStorage.push(product);
-      localStorage.setItem("item", JSON.stringify(productInLocalStorage));
-  } else {
-      const foundOptionProduct = productInLocalStorage.find(element => element.id == product.id && element.couleur == product.couleur);
+// function saveInLocalStorage(product) {
+//   let productInLocalStorage = JSON.parse(localStorage.getItem("item"));
+//   if (productInLocalStorage === null) {
+//       productInLocalStorage= [];
+//       productInLocalStorage.push(product);
+//       localStorage.setItem("item", JSON.stringify(productInLocalStorage));
+//   } else {
+//       const foundOptionProduct = productInLocalStorage.find(element => element.id == product.id && element.couleur == product.couleur);
       
-      if (foundOptionProduct == undefined) {
-          productInLocalStorage.push(product);
-          localStorage.setItem("item", JSON.stringify(productInLocalStorage));
+//       if (foundOptionProduct == undefined) {
+//           productInLocalStorage.push(product);
+//           localStorage.setItem("item", JSON.stringify(productInLocalStorage));
 
-      } else {
-          foundOptionProduct.quantite += product.quantite;
-          localStorage.setItem("item", JSON.stringify(productInLocalStorage));
-      }
-  }   
-}
+//       } else {
+//           foundOptionProduct.quantite += product.quantite;
+//           localStorage.setItem("item", JSON.stringify(productInLocalStorage));
+//       }
+//   }   
+// }
 
-const addBasket = () => {
+const addCart = (product) => {
+
   let button = document.getElementById("addToCart")
   console.log(button)
   button.addEventListener("click", () => {
@@ -66,17 +70,38 @@ const addBasket = () => {
     const colorsValue = document.querySelector("#colors").value
     const quantityValue = parseInt(document.querySelector("#quantity").value)
 
-  let productOptions = {
-    id: `${productData._id}`,
-    nom: `${productData.name}`,
-    couleur: colorsValue ,
-    quantite: quantityValue ,
-    prix: `${productData.price}`,
-    image: `${productData.imageUrl}`,
-    alt: `${productData.altTxt}`
-}
-saveInLocalStorage(productOptions);
-  })}
+    let newItem = {
+      _id: productId,
+      imageUrl: product.imageUrl,
+      name: product.name,
+      price: product.price,
+      selectColors: colorsValue,
+      quantity: quantityValue,
+  }
 
+  let itemOptions = {
+    id: `${product._id}`,
+    name: `${product.name}`,
+    colors: colorsValue ,
+    quantity: quantityValue ,
+    price: `${product.price}`,
+    image: `${product.imageUrl}`,
+    altTxt: `${product.altTxt}`
+}
+
+let index = item.findIndex(itemOptions => itemOptions['_id'] == productId);
+let indexOption= item.findIndex(newItem => newItem['selectColors'] == colorsValue);
+
+if(index === -1){
+    item.push(itemOptions);
+        }else if(indexOption === -1){
+            item.push(newItem);
+    }else {
+            item[index].quantity = item[index].quantity + Number(itemQuantity.value);
+        }
+        localStorage.setItem("item", JSON.stringify(item));
+
+});
+}
 
 
